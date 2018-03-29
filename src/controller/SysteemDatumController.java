d08ae92fdf9ac3f3cbf8f49a94f48d6e329e0339
BeingBright <BrightonvandenEnd@gmail.com>
2018-03-29 13:18:06 +0200

    implement Utils

--- src/controller/SysteemDatumController.java
+++ src/controller/SysteemDatumController.java
@@ -1,48 +1,48 @@
 package controller;
 
-import java.util.Calendar;
-
-import javax.json.Json;
-import javax.json.JsonObjectBuilder;
-
+import Utils.Utils;
 import model.PrIS;
 import server.Conversation;
 import server.Handler;
 
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import java.util.Calendar;
+
 public class SysteemDatumController implements Handler {
 
-	/**
-	 * De SysteemDatumController klasse moet alle systeem (en test)-gerelateerde aanvragen
-	 * afhandelen. Methode handle() kijkt welke URI is opgevraagd en laat
-	 * dan de juiste methode het werk doen. Je kunt voor elke nieuwe URI
-	 * een nieuwe methode schrijven.
-	 *
-	 * @param infoSys - het toegangspunt tot het domeinmodel
-	 */
-	public SysteemDatumController(PrIS infoSys) {
-	}
+    /**
+     * De SysteemDatumController klasse moet alle systeem (en test)-gerelateerde aanvragen
+     * afhandelen. Methode handle() kijkt welke URI is opgevraagd en laat
+     * dan de juiste methode het werk doen. Je kunt voor elke nieuwe URI
+     * een nieuwe methode schrijven.
+     *
+     * @param infoSys - het toegangspunt tot het domeinmodel
+     */
+    public SysteemDatumController(PrIS infoSys) {
+    }
 
-	public void handle(Conversation conversation) {
-		if (conversation.getRequestedURI().startsWith("/systeemdatum/lesinfo")) {
-			ophalenLesInfo(conversation);
-		}
-	}
+    public void handle(Conversation conversation) {
+        if (conversation.getRequestedURI().startsWith("/systeemdatum/lesinfo")) {
+            ophalenLesInfo(conversation);
+        }
+    }
 
-	private void ophalenLesInfo(Conversation conversation) {
-		//<to do> begin
-		//De volgende statements moeten gewijzigd worden zodat daadwerkelijk de eerste en laatste lesdatum wordt bepaald
-		Calendar lEersteLesDatum = Calendar.getInstance();
-		Calendar lLaatsteLesDatum = Calendar.getInstance();
-		//<to do> end
+    private void ophalenLesInfo(Conversation conversation) {
+        //<to do> begin
+        //De volgende statements moeten gewijzigd worden zodat daadwerkelijk de eerste en laatste lesdatum wordt bepaald
+        Calendar lEersteLesDatum = Calendar.getInstance();
+        Calendar lLaatsteLesDatum = Calendar.getInstance();
+        //<to do> end
 
-		JsonObjectBuilder lJsonObjectBuilder = Json.createObjectBuilder();
-		//Deze volgorde mag niet worden gewijzigd i.v.m. JS. (Hier mag dus ook geen andere JSON voor komen.)
-		lJsonObjectBuilder
-				.add("eerste_lesdatum", PrIS.calToStandaardDatumString(lEersteLesDatum))
-				.add("laatste_lesdatum", PrIS.calToStandaardDatumString(lLaatsteLesDatum));
+        JsonObjectBuilder lJsonObjectBuilder = Json.createObjectBuilder();
+        //Deze volgorde mag niet worden gewijzigd i.v.m. JS. (Hier mag dus ook geen andere JSON voor komen.)
+        lJsonObjectBuilder
+                .add("eerste_lesdatum", Utils.calToStandaardDatumString(lEersteLesDatum))
+                .add("laatste_lesdatum", Utils.calToStandaardDatumString(lLaatsteLesDatum));
 
-		String lJsonOut = lJsonObjectBuilder.build().toString();
+        String lJsonOut = lJsonObjectBuilder.build().toString();
 
-		conversation.sendJSONMessage(lJsonOut);										// terugsturen naar de Polymer-GUI!	}
-	}
+        conversation.sendJSONMessage(lJsonOut);                                        // terugsturen naar de Polymer-GUI!	}
+    }
 }
--- src/model/PrIS.java
+++ src/model/PrIS.java
@@ -1,225 +1,186 @@
 package model;
 
-import java.util.ArrayList;
-
 import model.person.Person;
-import model.person.Teacher;
 import model.person.Student;
+import model.person.Teacher;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-
-
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.ArrayList;
 
 public class PrIS {
-	private ArrayList<Teacher> teachers;
-	private ArrayList<Student> students;
-	private ArrayList<Group> groups;
-	private ArrayList<Person> people;
+    private ArrayList<Teacher> teachers;
+    private ArrayList<Student> students;
+    private ArrayList<Group> groups;
+    private ArrayList<Person> people;
 
-	/**
-	 * De constructor maakt een set met standaard-data aan. Deze data
-	 * moet nog uitgebreidt worden met rooster gegevens die uit een bestand worden
-	 * ingelezen, maar dat is geen onderdeel van deze demo-applicatie!
-	 *
-	 * De klasse PrIS (PresentieInformatieSysteem) heeft nu een meervoudige
-	 * associatie met de klassen Docent, Student, Vakken en Klassen Uiteraard kan dit nog veel
-	 * verder uitgebreid en aangepast worden! 
-	 *
-	 * De klasse fungeert min of meer als ingangspunt voor het domeinmodel. Op
-	 * dit moment zijn de volgende methoden aanroepbaar:
-	 *
-	 * String login(String gebruikersnaam, String wachtwoord)
-	 * Docent getDocent(String gebruikersnaam)
-	 * Student getStudent(String gebruikersnaam)
-	 * ArrayList<Student> getStudentenVanKlas(String klasCode)
-	 *
-	 * Methode login geeft de rol van de gebruiker die probeert in te loggen,
-	 * dat kan 'student', 'docent' of 'undefined' zijn! Die informatie kan gebruikt 
-	 * worden om in de Polymer-GUI te bepalen wat het volgende scherm is dat getoond 
-	 * moet worden.
-	 *
-	 */
-	public PrIS() {
-		teachers = new ArrayList<>();
-		students = new ArrayList<>();
-		groups = new ArrayList<>();
-		people = new ArrayList<>();
+    /**
+     * De constructor maakt een set met standaard-data aan. Deze data
+     * moet nog uitgebreidt worden met rooster gegevens die uit een bestand worden
+     * ingelezen, maar dat is geen onderdeel van deze demo-applicatie!
+     * <p>
+     * De klasse PrIS (PresentieInformatieSysteem) heeft nu een meervoudige
+     * associatie met de klassen Docent, Student, Vakken en Klassen Uiteraard kan dit nog veel
+     * verder uitgebreid en aangepast worden!
+     * <p>
+     * De klasse fungeert min of meer als ingangspunt voor het domeinmodel. Op
+     * dit moment zijn de volgende methoden aanroepbaar:
+     * <p>
+     * String login(String gebruikersnaam, String wachtwoord)
+     * Docent getDocent(String gebruikersnaam)
+     * Student getStudent(String gebruikersnaam)
+     * ArrayList<Student> getStudentenVanKlas(String klasCode)
+     * <p>
+     * Methode login geeft de rol van de gebruiker die probeert in te loggen,
+     * dat kan 'student', 'docent' of 'undefined' zijn! Die informatie kan gebruikt
+     * worden om in de Polymer-GUI te bepalen wat het volgende scherm is dat getoond
+     * moet worden.
+     */
+    public PrIS() {
+        teachers = new ArrayList<>();
+        students = new ArrayList<>();
+        groups = new ArrayList<>();
+        people = new ArrayList<>();
 
-		fillGroup();
-		fillStudents();
-		fillTeachers();
+        fillGroup();
+        fillStudents();
+        fillTeachers();
 
 //		TODO: ADD BETTER LOADING
-		people.addAll(students);
-		people.addAll(teachers);
-	}
+        people.addAll(students);
+        people.addAll(teachers);
+    }
 
-	//deze method is static onderdeel van PrIS omdat hij als hulp methode 
-	//in veel controllers gebruikt wordt
-	//een standaardDatumString heeft formaat YYYY-MM-DD
-	public static Calendar standaardDatumStringToCal(String pStadaardDatumString) {
-		Calendar lCal = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		try {
-			lCal.setTime(sdf.parse(pStadaardDatumString));
-		}  catch (ParseException e) {
-			e.printStackTrace();
-			lCal=null;
-		}
-		return lCal;
-	}
-	//deze method is static onderdeel van PrIS omdat hij als hulp methode 
-	//in veel controllers gebruikt wordt
-	//een standaardDatumString heeft formaat YYYY-MM-DD
-	public static String calToStandaardDatumString (Calendar pCalendar) {
-		int lJaar = pCalendar.get(Calendar.YEAR);
-		int lMaand= pCalendar.get(Calendar.MONTH) + 1;
-		int lDag  = pCalendar.get(Calendar.DAY_OF_MONTH);
+    public Teacher getTeacher(String username) {
+        for (Teacher teacher : this.teachers) {
+            if (teacher.getUsername().equals(username)) {
+                return teacher;
+            }
+        }
+        return null;
+    }
 
-		String lMaandStr = Integer.toString(lMaand);
-		if (lMaandStr.length() == 1) {
-			lMaandStr = "0"+ lMaandStr;
-		}
-		String lDagStr = Integer.toString(lDag);
-		if (lDagStr.length() == 1) {
-			lDagStr = "0"+ lDagStr;
-		}
-		return Integer.toString(lJaar) + "-" + lMaandStr + "-" + lDagStr;
-	}
+    public Group getStudentGroup(Student student) {
+        for (Group group : this.groups) {
+            if (group.hasStudent(student)) {
+                return (group);
+            }
+        }
+        return null;
+    }
 
-	public Teacher getTeacher(String username) {
-		for (Teacher teacher: this.teachers) {
-			if (teacher.getUsername().equals(username)) {
-				return teacher;
-			}
-		}
-		return null;
-	}
+    public Student getStudent(String username) {
+        for (Student student : this.students) {
+            if (student.getUsername().equals(username)) {
+                return student;
+            }
+        }
+        return null;
+    }
 
-	public Group getStudentGroup(Student student) {
-		for (Group group: this.groups) {
-			if (group.hasStudent(student)){
-				return (group);
-			}
-		}
-		return null;
-	}
+    public Student getStudent(int studentId) {
+        for (Student student : this.students) {
+            if (student.getStudentId() == studentId) {
+                return student;
+            }
+        }
+        return null;
+    }
 
-	public Student getStudent(String username) {
-		for (Student student: this.students) {
-			if (student.getUsername().equals(username)) {
-				return student;
-			}
-		}
-		return null;
-	}
+    public String login(String username, String password) {
+        for (Person person : people) {
+            if (person.getUsername().equals(username)) {
+                if (person.samePassword(password)) {
+                    if (person instanceof Teacher) {
+                        return "docent";
+                    } else if (person instanceof Student) {
+                        return "student";
+                    }
+                }
+                return "incorrect password";
+            }
+        }
+        return "username not found";
+    }
 
-	public Student getStudent(int studentId) {
-		for (Student student: this.students) {
-			if (student.getStudentId() == studentId) {
-				return student;
-			}
-		}
-		return null;
-	}
+    private void fillTeachers() {
+        String csvFile = "././CSV/docenten.csv";
+        BufferedReader br = null;
+        String line;
+        String cvsSplitBy = ",";
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+                String[] element = line.split(cvsSplitBy);
+                String username = element[0].toLowerCase();
+                String firstName = element[1];
+                String middleName = element[2];
+                String lastName = element[3];
+                this.teachers.add(new Teacher(firstName, middleName, lastName, "geheim", username, 1));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
-	public String login(String username, String password) {
-		for (Person person: people) {
-			if (person.getUsername().equals(username)) {
-				if (person.samePassword(password)) {
-					if (person instanceof Teacher) {
-						return "docent";
-					} else if (person instanceof Student) {
-						return "student";
-					}
-				}
-				return "incorrect password";
-			}
-		}
-		return "username not found";
-	}
+    private void fillGroup() {
+        Group k2 = new Group("TICT-SIE-V1B", "V1B");
+        Group k3 = new Group("TICT-SIE-V1C", "V1C");
+        Group k4 = new Group("TICT-SIE-V1D", "V1D");
+        Group k5 = new Group("TICT-SIE-V1E", "V1E");
+        Group k6 = new Group("TICT-SIE-V1F", "V1F");
 
-	private void fillTeachers() {
-		String csvFile = "././CSV/docenten.csv";
-		BufferedReader br = null;
-		String line;
-		String cvsSplitBy = ",";
-		try {
-			br = new BufferedReader(new FileReader(csvFile));
-			while ((line = br.readLine()) != null) {
-				String[] element = line.split(cvsSplitBy);
-				String username = element[0].toLowerCase();
-				String firstName = element[1];
-				String middleName = element[2];
-				String lastName = element[3];
-				this.teachers.add(new Teacher(firstName, middleName, lastName, "geheim", username, 1));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+        //pKlassen.add(k1);
+        this.groups.add(k2);
+        this.groups.add(k3);
+        this.groups.add(k4);
+        this.groups.add(k5);
+        this.groups.add(k6);
+    }
 
-	private void fillGroup() {
-		Group k2 = new Group("TICT-SIE-V1B", "V1B");
-		Group k3 = new Group("TICT-SIE-V1C", "V1C");
-		Group k4 = new Group("TICT-SIE-V1D", "V1D");
-		Group k5 = new Group("TICT-SIE-V1E", "V1E");
-		Group k6 = new Group("TICT-SIE-V1F", "V1F");
+    private void fillStudents() {
+        Student lStudent;
+        for (Group group : this.groups) {
+            String csvFile = "././CSV/" + group.getName() + ".csv";
+            BufferedReader br = null;
+            String line;
+            String cvsSplitBy = ",";
+            try {
+                br = new BufferedReader(new FileReader(csvFile));
+                while ((line = br.readLine()) != null) {
+                    //line = line.replace(",,", ", ,");
+                    // use comma as separator
+                    String[] element = line.split(cvsSplitBy);
+                    String gebruikersnaam = (element[3] + "." + element[2] + element[1] + "@student.hu.nl").toLowerCase();
+                    // verwijder spaties tussen  dubbele voornamen en tussen bv van der
+                    gebruikersnaam = gebruikersnaam.replace(" ", "");
+                    String lStudentNrString = element[0];
+                    int lStudentNr = Integer.parseInt(lStudentNrString);
+                    lStudent = new Student(element[3], element[2], element[1], "geheim", gebruikersnaam, lStudentNr); //Volgorde 3-2-1 = voornaam, tussenvoegsel en achternaam
+                    this.students.add(lStudent);
+                    group.addStudent(lStudent);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
-		//pKlassen.add(k1);
-		this.groups.add(k2);
-		this.groups.add(k3);
-		this.groups.add(k4);
-		this.groups.add(k5);
-		this.groups.add(k6);
-	}
-	private void fillStudents() {
-		Student lStudent;
-		for (Group group: this.groups) {
-			String csvFile = "././CSV/" + group.getName() + ".csv";
-			BufferedReader br = null;
-			String line;
-			String cvsSplitBy = ",";
-			try {
-				br = new BufferedReader(new FileReader(csvFile));
-				while ((line = br.readLine()) != null) {
-					//line = line.replace(",,", ", ,");
-					// use comma as separator
-					String[] element = line.split(cvsSplitBy);
-					String gebruikersnaam = (element[3] + "." + element[2] + element[1] + "@student.hu.nl").toLowerCase();
-					// verwijder spaties tussen  dubbele voornamen en tussen bv van der 
-					gebruikersnaam = gebruikersnaam.replace(" ","");
-					String lStudentNrString  = element[0];
-					int lStudentNr = Integer.parseInt(lStudentNrString);
-					lStudent = new Student(element[3], element[2], element[1], "geheim", gebruikersnaam, lStudentNr); //Volgorde 3-2-1 = voornaam, tussenvoegsel en achternaam
-					this.students.add(lStudent);
-					group.addStudent(lStudent);
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				if (br != null) {
-					try {
-						br.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-
-		}
-	}
+        }
+    }
 }
