373a2dbe6c78a504772ebddf0983a011ee383639
Robert-Jan Keizer <robertjankeizer@gmail.com>
2018-04-10 12:38:18 +0200

    Update presentielijst

--- src/controller/TeacherController.java
+++ src/controller/TeacherController.java
@@ -13,7 +13,7 @@ import javax.json.JsonObject;
 public class TeacherController implements Handler {
     private PrIS informatieSysteem;
 
-    public static String ROUTE_PRESENTION = "/presentie/";
+    public static String ROUTE_PRESENTION = "/presentie";
 
     public TeacherController(PrIS informatieSysteem) {
         this.informatieSysteem = informatieSysteem;
@@ -27,7 +27,7 @@ public class TeacherController implements Handler {
 
     public void handlePresent(Conversation conversation) {
         JsonObject responseObject = (JsonObject) conversation.getRequestBodyAsJSON();
-        Lesson lesson = informatieSysteem.getLesson(responseObject.getInt("lessonId"));
+        Lesson lesson = informatieSysteem.getLesson(Integer.valueOf(responseObject.getString("lessonId")));
         for (Student student: lesson.getGroup().getStudents()) {
             lesson.addPresention(new Presention(student, lesson, false));
         }
