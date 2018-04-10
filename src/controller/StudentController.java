85f40958007a104e6af39c605c775c482269ee88
Joostlek <joostlek@outlook.com>
2018-04-10 15:49:57 +0200

    ;)

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -52,10 +52,10 @@ public class StudentController implements Handler {
 
         System.out.println(student.getGroupId());
         for (Lesson lesson : group.getLessons()) {
-            presentResponse.add(new LessonResponse(lesson));
+            presentResponse.add(lesson.toLessonResponse());
         }
 
-        StudentResponse studentResponse = new StudentResponse(student);
+        StudentResponse studentResponse = student.toStudentResponse();
         studentResponse.setLessons(presentResponse);
 
         Gson gson = new Gson();
