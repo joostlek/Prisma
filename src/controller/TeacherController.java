42ec31df101002ab366b6b5a7f0898849cdeac56
Robert-Jan Keizer <robertjankeizer@gmail.com>
2018-04-10 15:31:28 +0200

    Update searchcontroller

--- src/controller/TeacherController.java
+++ src/controller/TeacherController.java
@@ -30,7 +30,7 @@ public class TeacherController implements Handler {
 
     public void handlePresent(Conversation conversation) {
         JsonObject responseObject = (JsonObject) conversation.getRequestBodyAsJSON();
-        Lesson lesson = informatieSysteem.getLesson(responseObject.getInt("lessonId"));
+        Lesson lesson = informatieSysteem.getLesson(Integer.parseInt(responseObject.getString("lessonId")));
         if (lesson.getPresentions().size() == 0) {
             for (Student student: lesson.getGroup().getStudents()) {
                 lesson.addPresention(new Presention(student, lesson, false));
@@ -42,7 +42,7 @@ public class TeacherController implements Handler {
 
     public void handleSave(Conversation conversation) {
         JsonObject responseObject = (JsonObject) conversation.getRequestBodyAsJSON();
-        Lesson lesson = informatieSysteem.getLesson(responseObject.getInt("lessonId"));
+        Lesson lesson = informatieSysteem.getLesson(Integer.parseInt(responseObject.getString("lessonId")));
 
         JsonObject presenties = responseObject.getJsonObject("presenties");
         for (Presention presention: lesson.getPresentions()) {
